c57628aae0da4e5039ad2b57d4c56d11a34edb1b
jnn121 <111894253+jnn121@users.noreply.github.com>
2025-06-10 10:53:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
+
+const corsAnywhere = require('cors-anywhere');
+const host = '0.0.0.0';
+const port = process.env.PORT || 8080;
+
+corsAnywhere.createServer({
+  // 👇 Disable header requirement
+  requireHeader: [], 
+  // 👇 Keep these empty to allow all headers/CORS
+  removeHeaders: [],
+  originWhitelist: [], // Allow all origins
+}).listen(port, host, () => {
+  console.log('CORS Proxy running on ' + host + ':' + port);
+});
+
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
